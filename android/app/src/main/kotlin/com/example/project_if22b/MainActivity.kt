d7d7fdc49bee1c25b3f7d40d7f729935c50330fb
reflan nuari <reflannuari@gmail.com>
2024-04-27 15:13:10 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/project_if22b/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_if22b
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/config/asset.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Asset {
+  static Color colorPrimaryDark = Color.fromARGB(255, 2, 84, 100);
+  static Color colorPrimary = Color.fromARGB(255, 2229, 124, 35);
+  static Color colorSecoundary = Color.fromARGB(255, 232, 170, 6);
+  static Color colorAccent = Color.fromARGB(255, 115, 18, 32);
+}
